2a6c5c143a657fb8946aa4ae6e54d7732b033a89
xChevy <chevyself@gmail.com>
2020-05-28 20:17:52 -0500

    Add default messages provider for bungee and more result constructors

--- bungee/src/main/java/com/starfishst/bungee/result/Result.java
+++ bungee/src/main/java/com/starfishst/bungee/result/Result.java
@@ -1,17 +1,34 @@
 package com.starfishst.bungee.result;
 
 import com.starfishst.core.result.SimpleResult;
+import com.starfishst.core.utils.Strings;
+import java.util.HashMap;
 import org.jetbrains.annotations.NotNull;
 
 /** Result for bungee command execution */
 public class Result extends SimpleResult {
 
   /**
-   * Create a new instance of {@link SimpleResult} with a message with place holders
+   * A result with a message
    *
-   * @param string the message with place holders
+   * @param string the message to send as result
    */
   public Result(@NotNull String string) {
     super(string);
+  }
+
+  /**
+   * A result with a message that contains placeholders
+   *
+   * @param string the message to send as result
+   * @param placeholders the placeholders to change in the message
+   */
+  public Result(@NotNull String string, @NotNull HashMap<String, String> placeholders) {
+    this(Strings.buildMessage(string, placeholders));
+  }
+
+  /** A result without message */
+  public Result() {
+    super();
   }
 }
