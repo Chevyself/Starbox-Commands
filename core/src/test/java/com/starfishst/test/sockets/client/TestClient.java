465f5fa4807b5529379e6568b47b3612cc0fa5e1
xChevy <chevyself@gmail.com>
2020-05-30 19:52:59 -0500

    Create tests for sockets

--- core/src/test/java/com/starfishst/test/sockets/client/TestClient.java
+++ core/src/test/java/com/starfishst/test/sockets/client/TestClient.java
@@ -4,17 +4,16 @@ import com.starfishst.core.utils.sockets.client.Client;
 import com.starfishst.core.utils.sockets.messaging.handlers.ResponseGiver;
 import com.starfishst.core.utils.sockets.messaging.handlers.type.DisconnectedHandler;
 import com.starfishst.core.utils.sockets.messaging.type.request.PingRequest;
-
 import java.io.IOException;
 
 public class TestClient {
 
-    public static void main(String[] args) throws IOException {
-        Client client = new Client("localhost", 3000);
-        ResponseGiver.addHandler(new DisconnectedHandler(client));
-        System.out.println(client.sendRequest(new PingRequest()).getData().getOrDefault("end", "0"));
-        while (true) {
-            client.listen();
-        }
+  public static void main(String[] args) throws IOException {
+    Client client = new Client("localhost", 3000);
+    ResponseGiver.addHandler(new DisconnectedHandler(client));
+    System.out.println(client.sendRequest(new PingRequest()).getData().getOrDefault("end", "0"));
+    while (true) {
+      client.listen();
     }
+  }
 }
--- core/src/test/java/com/starfishst/test/sockets/server/TestServer.java
+++ core/src/test/java/com/starfishst/test/sockets/server/TestServer.java
@@ -6,29 +6,29 @@ import com.starfishst.core.utils.sockets.messaging.handlers.type.PingHandler;
 import com.starfishst.core.utils.sockets.server.ClientThread;
 import com.starfishst.core.utils.sockets.server.Server;
 import com.starfishst.core.utils.time.Time;
-import org.jetbrains.annotations.NotNull;
-
 import java.io.IOException;
+import org.jetbrains.annotations.NotNull;
 
 public class TestServer {
 
-    public static void main(String[] args) throws IOException {
-        Server server = new Server(3000, Time.fromString("15s")) {
-            @Override
-            public void startClientTask(@NotNull ClientThread client) {
-                while (true) {
-                    try {
-                        client.listen();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+  public static void main(String[] args) throws IOException {
+    Server server =
+        new Server(3000, Time.fromString("15s")) {
+          @Override
+          public void startClientTask(@NotNull ClientThread client) {
+            while (true) {
+              try {
+                client.listen();
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
             }
+          }
         };
-        ResponseGiver.addHandler(new DisconnectHandler(server));
-        ResponseGiver.addHandler(new PingHandler());
-        while (true) {
-            server.run();
-        }
+    ResponseGiver.addHandler(new DisconnectHandler(server));
+    ResponseGiver.addHandler(new PingHandler());
+    while (true) {
+      server.run();
     }
+  }
 }
