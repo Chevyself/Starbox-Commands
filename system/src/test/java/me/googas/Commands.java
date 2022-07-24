e2cbc88f30e0335fbad95e77e62c53bd3154a417
Chevyself <chevyself@gmail.com>
2022-07-24 14:39:18 -0500

    Tests for argument behaviour

--- system/src/test/java/me/googas/Commands.java
+++ system/src/test/java/me/googas/Commands.java
@@ -3,6 +3,7 @@ package me.googas;
 import me.googas.commands.annotations.Free;
 import me.googas.commands.annotations.Multiple;
 import me.googas.commands.annotations.Required;
+import me.googas.commands.arguments.ArgumentBehaviour;
 import me.googas.commands.flags.Flag;
 import me.googas.commands.system.Command;
 import me.googas.commands.system.Result;
@@ -12,9 +13,29 @@ import me.googas.commands.time.annotations.TimeAmount;
 @SuppressWarnings("JavaDoc")
 public class Commands {
 
-  @Command(aliases = "a")
-  public Result a(@Required String b, @Required @Multiple String c) {
-    return new Result("b = " + b + ", c = " + c);
+  @Command(
+      aliases = "a",
+      options = {@Flag(aliases = "message", value = "Hello world!")})
+  public Result a(
+      CommandContext context,
+      @Required(name = "b", behaviour = ArgumentBehaviour.MULTIPLE) String b,
+      @Required(name = "c", behaviour = ArgumentBehaviour.MULTIPLE) String c,
+      @Required(name = "d") int number,
+      @Required(name = "e") int number2,
+      @Free(behaviour = ArgumentBehaviour.CONTINUOUS) String extra) {
+    return new Result(
+        "b = "
+            + b
+            + ", c = "
+            + c
+            + ", d = "
+            + number
+            + ", e = "
+            + number2
+            + ", message = "
+            + context.getFlagValue("message")
+            + ", "
+            + extra);
   }
 
   @Command(aliases = "message")
@@ -28,5 +49,13 @@ public class Commands {
       options = @Flag(aliases = "message", value = "World!"))
   public Result hello(CommandContext context) {
     return new Result(context.getFlagValue("message").orElse("IDK :(")).setCooldown(true);
+  }
+
+  @Command(aliases = "trollface")
+  public void trollface(
+      @Required(name = "message") String message, @Required(name = "number") int number) {
+    for (int i = 0; i < number; i++) {
+      System.out.println(message);
+    }
   }
 }
