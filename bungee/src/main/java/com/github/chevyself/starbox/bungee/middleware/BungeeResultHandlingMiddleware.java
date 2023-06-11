be477731c77712ed9944d6331a3d2733f67ba47f
Chevyself <chevyself@gmail.com>
2023-06-11 15:40:07 -0500

    Remove unused constructor from AbstractAnnotatedCommand

---
+++ bungee/src/main/java/com/github/chevyself/starbox/bungee/middleware/BungeeResultHandlingMiddleware.java
@@ -0,0 +1,47 @@
+package com.github.chevyself.starbox.bungee.middleware;
+
+import com.github.chevyself.starbox.bungee.context.CommandContext;
+import com.github.chevyself.starbox.bungee.result.BungeeResult;
+import com.github.chevyself.starbox.common.Components;
+import com.github.chevyself.starbox.middleware.ResultHandlingMiddleware;
+import com.github.chevyself.starbox.result.ArgumentExceptionResult;
+import com.github.chevyself.starbox.result.InternalExceptionResult;
+import com.github.chevyself.starbox.result.SimpleResult;
+import com.github.chevyself.starbox.result.StarboxResult;
+import lombok.NonNull;
+import net.md_5.bungee.api.chat.BaseComponent;
+
+/**
+ * This middleware sends the components of the {@link BungeeResult} to the {@link
+ * net.md_5.bungee.api.CommandSender}.
+ */
+public class BungeeResultHandlingMiddleware extends ResultHandlingMiddleware<CommandContext>
+    implements BungeeMiddleware {
+
+  @Override
+  public void next(@NonNull CommandContext context, StarboxResult result) {
+    super.next(context, result);
+    if (result instanceof BungeeResult && !((BungeeResult) result).getComponents().isEmpty()) {
+      context
+          .getSender()
+          .sendMessage(((BungeeResult) result).getComponents().toArray(new BaseComponent[0]));
+    }
+  }
+
+  @Override
+  public void onException(
+      @NonNull CommandContext context, @NonNull InternalExceptionResult result) {
+    context.getSender().sendMessage(Components.getComponent(result.getMessage()));
+  }
+
+  @Override
+  public void onException(
+      @NonNull CommandContext context, @NonNull ArgumentExceptionResult result) {
+    context.getSender().sendMessage(Components.getComponent(result.getMessage()));
+  }
+
+  @Override
+  public void onSimple(@NonNull CommandContext context, @NonNull SimpleResult result) {
+    context.getSender().sendMessage(Components.getComponent(result.getMessage()));
+  }
+}
--- core/src/main/java/com/github/chevyself/starbox/commands/AbstractAnnotatedCommand.java
+++ core/src/main/java/com/github/chevyself/starbox/commands/AbstractAnnotatedCommand.java
@@ -6,10 +6,6 @@ import com.github.chevyself.starbox.arguments.Argument;
 import com.github.chevyself.starbox.context.StarboxCommandContext;
 import com.github.chevyself.starbox.exceptions.ArgumentProviderException;
 import com.github.chevyself.starbox.exceptions.MissingArgumentException;
-import com.github.chevyself.starbox.flags.Option;
-import com.github.chevyself.starbox.messages.MessagesProvider;
-import com.github.chevyself.starbox.middleware.Middleware;
-import com.github.chevyself.starbox.registry.ProvidersRegistry;
 import com.github.chevyself.starbox.result.ArgumentExceptionResult;
 import com.github.chevyself.starbox.result.InternalExceptionResult;
 import com.github.chevyself.starbox.result.StarboxResult;
@@ -26,23 +22,6 @@ public abstract class AbstractAnnotatedCommand<
   @NonNull @Getter protected final Object object;
   @NonNull @Getter protected final Method method;
   @NonNull @Getter protected final List<Argument<?>> arguments;
-
-  private AbstractAnnotatedCommand(
-      @NonNull CommandManager<C, T> commandManager,
-      @NonNull Object object,
-      @NonNull Method method,
-      @NonNull List<Argument<?>> arguments,
-      @NonNull ProvidersRegistry<C> providersRegistry,
-      @NonNull MessagesProvider<C> messagesProvider,
-      @NonNull List<String> aliases,
-      @NonNull List<Middleware<C>> middlewares,
-      @NonNull List<Option> options,
-      @NonNull List<T> children) {
-    super(commandManager, aliases, middlewares, options, children);
-    this.object = object;
-    this.method = method;
-    this.arguments = arguments;
-  }
 
   protected AbstractAnnotatedCommand(
       @NonNull CommandManager<C, T> commandManager,
