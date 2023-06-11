f7c8ea060c1e8507a10d06e83ea67a968c9c331a
Chevyself <chevyself@gmail.com>
2023-06-10 19:24:21 -0500

    Add result handling middleware

---
+++ core/src/main/java/com/github/chevyself/starbox/middleware/ResultHandlingMiddleware.java
@@ -0,0 +1,24 @@
+package com.github.chevyself.starbox.middleware;
+
+import com.github.chevyself.starbox.commands.StarboxCommand;
+import com.github.chevyself.starbox.context.StarboxCommandContext;
+import com.github.chevyself.starbox.result.ExceptionResult;
+import com.github.chevyself.starbox.result.SimpleResult;
+import com.github.chevyself.starbox.result.StarboxResult;
+import lombok.NonNull;
+
+public abstract class ResultHandlingMiddleware<C extends StarboxCommandContext<C, ?>> implements Middleware<C> {
+
+  @Override
+  public void next(@NonNull C context, StarboxResult result) {
+    if (result instanceof SimpleResult) {
+      this.onSimple(result);
+    } else if (result instanceof ExceptionResult) {
+      this.onException(context, (ExceptionResult) result);
+    }
+  }
+
+  public abstract void onException(@NonNull C context, @NonNull ExceptionResult result);
+
+  public abstract void onSimple(@NonNull StarboxResult result);
+}
--- core/src/main/java/com/github/chevyself/starbox/result/ArgumentExceptionResult.java
+++ core/src/main/java/com/github/chevyself/starbox/result/ArgumentExceptionResult.java
@@ -3,7 +3,7 @@ package com.github.chevyself.starbox.result;
 import com.github.chevyself.starbox.exceptions.type.StarboxException;
 import lombok.NonNull;
 
-public class ArgumentExceptionResult extends ExceptionResult{
+public class ArgumentExceptionResult extends ExceptionResult {
 
   public ArgumentExceptionResult(@NonNull StarboxException exception) {
     super(exception);
