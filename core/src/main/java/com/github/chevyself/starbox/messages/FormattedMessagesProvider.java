644fa1210ddc6e62f3f73fea10d96906378d5489
Chevyself <chevyself@gmail.com>
2023-06-12 00:50:24 -0500

    Implement FormattedMessagesProvider

---
+++ core/src/main/java/com/github/chevyself/starbox/messages/FormattedMessagesProvider.java
@@ -0,0 +1,64 @@
+package com.github.chevyself.starbox.messages;
+
+import com.github.chevyself.starbox.context.StarboxCommandContext;
+import com.github.chevyself.starbox.time.TimeUtil;
+import java.time.Duration;
+import lombok.NonNull;
+
+public abstract class FormattedMessagesProvider<C extends StarboxCommandContext<C, ?>>
+    implements MessagesProvider<C> {
+
+  @NonNull
+  public abstract String element();
+
+  @NonNull
+  public abstract String text();
+
+  @Override
+  public @NonNull String invalidLong(@NonNull String string, @NonNull C context) {
+    return String.format("%s%s %sis not a valid long!", this.element(), string, this.text());
+  }
+
+  @Override
+  public @NonNull String invalidInteger(@NonNull String string, @NonNull C context) {
+    return String.format("%s%s %sis not a valid integer!", this.element(), string, this.text());
+  }
+
+  @Override
+  public @NonNull String invalidDouble(@NonNull String string, @NonNull C context) {
+    return String.format("%s%s %sis not a valid double!", this.element(), string, this.text());
+  }
+
+  @Override
+  public @NonNull String invalidBoolean(@NonNull String string, @NonNull C context) {
+    return String.format("%s%s %sis not a valid boolean!", this.element(), string, this.text());
+  }
+
+  @Override
+  public @NonNull String invalidDuration(@NonNull String string, @NonNull C context) {
+    return String.format("%s%s %sis not valid time!", this.element(), string, this.text());
+  }
+
+  @Override
+  public @NonNull String missingArgument(
+      @NonNull String name, @NonNull String description, int position, C context) {
+    return this.text()
+        + "Missing the argument "
+        + this.element()
+        + name
+        + this.text()
+        + " ("
+        + this.element()
+        + description
+        + this.text()
+        + ") in "
+        + this.element()
+        + position;
+  }
+
+  @Override
+  public @NonNull String cooldown(@NonNull C context, @NonNull Duration timeLeft) {
+    return String.format(
+        "%sYou are not allowed to run this command for another %s%s", this.text(), this.element(), TimeUtil.toString(timeLeft));
+  }
+}
--- core/src/main/java/com/github/chevyself/starbox/messages/GenericMessagesProvider.java
+++ core/src/main/java/com/github/chevyself/starbox/messages/GenericMessagesProvider.java
@@ -7,41 +7,15 @@ import lombok.NonNull;
 
 /** The default {@link MessagesProvider} for System commands. */
 public class GenericMessagesProvider<C extends StarboxCommandContext<C, ?>>
-    implements MessagesProvider<C> {
+    extends FormattedMessagesProvider<C> {
 
   @Override
-  public @NonNull String invalidLong(@NonNull String string, @NonNull C context) {
-    return String.format("%s is not a valid long!", string);
+  public @NonNull String element() {
+    return "";
   }
 
   @Override
-  public @NonNull String invalidInteger(@NonNull String string, @NonNull C context) {
-    return String.format("%s is not a valid integer!", string);
-  }
-
-  @Override
-  public @NonNull String invalidDouble(@NonNull String string, @NonNull C context) {
-    return String.format("%s is not a valid double!", string);
-  }
-
-  @Override
-  public @NonNull String invalidBoolean(@NonNull String string, @NonNull C context) {
-    return String.format("%s is not a valid boolean!", string);
-  }
-
-  @Override
-  public @NonNull String invalidDuration(@NonNull String string, @NonNull C context) {
-    return String.format("%s is not valid time!", string);
-  }
-
-  @Override
-  public @NonNull String missingArgument(
-      @NonNull String name, @NonNull String description, int position, C context) {
-    return String.format("Missing the argument %s (%s) in %d", name, description, position);
-  }
-
-  @Override
-  public @NonNull String cooldown(@NonNull C context, @NonNull Duration timeLeft) {
-    return "You are not allowed to run this command for another " + TimeUtil.toString(timeLeft);
+  public @NonNull String text() {
+    return "";
   }
 }
